120b48d6b19199e4fd76a8bcf90e112ef6082610
Jon Fritzler <jon.fritzler@motorolasolutions.com>
2017-06-03 17:28:53 -0600

    Create setHiddenFields Function Make sure the hidden input answer's value is exactly 4 characters long Set the hidden input attempt's value to zero :)

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,12 @@ function guess() {
 }
 
 //implement new functions here
+function setHiddenFields(){
+    answer = Math.floor(Math.random() * 1000).toString();
+    while (answer.length < 4) {
+        answer = "0" + answer;
+    }
+
+    // reset the user attempts
+    attempt = 0;
+}
